2ba552680d95a92639fff29eba553a7722c9cdb1
M K <26510108+juggeroid@users.noreply.github.com>
2020-10-28 13:05:51 +0000

    Update zolotykh_task_48_simulation.cpp

--- zolotykh_task_48_simulation.cpp
+++ zolotykh_task_48_simulation.cpp
@@ -5,6 +5,10 @@
 #include <array>
 #include <random>
 
+// Let a, b be a points with coordinates (0, \theta_1), (1, \theta_2) respectively. It's known that \theta_1 ~ U[-1, 1], \theta_2 ~ U[-1, 1]. 
+// There's also a fixed point x = (0.32, 0). Find the probability P(d(b, x) < d(a, x)), where d(\dot, \dot) is a euclidean metric.
+// The analytic solution is available here: https://stats.stackexchange.com/questions/490357/probability-of-one-point-being-closer-to-fixed-x-in-mathbfr2-than-another
+
 namespace {
 	static constexpr auto ITERATIONS = 20'000'000;
 }
